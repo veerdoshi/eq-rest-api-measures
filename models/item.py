5ced209d503b2da956311a79cd5a052ab6941439
Veer Doshi <14866767+veerdoshi@users.noreply.github.com>
2017-12-25 16:45:56 -0800

    Add files via upload

---
+++ models/item.py
@@ -0,0 +1,35 @@
+from db import db
+
+class ItemModel(db.Model):
+    __tablename__ = 'items'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    price = db.Column(db.Float(precision=2))
+    lat = db.Column(db.Float(precision=4))
+    lon = db.Column(db.Float(precision=4))
+
+    store_id = db.Column(db.Integer, db.ForeignKey('stores.id'))
+    store = db.relationship('StoreModel')
+
+    def __init__(self, name, price, store_id, lat, lon):
+        self.name = name
+        self.price = price
+        self.lat = lat
+        self.lon = lon
+        self.store_id = store_id
+
+    def json(self):
+        return {'name': self.name, 'price': self.price, 'lat': self.lat, 'lon': self.lon}
+
+    @classmethod
+    def find_by_name(cls, name):
+        return cls.query.filter_by(name=name).first()
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
